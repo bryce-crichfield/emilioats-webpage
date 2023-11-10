b1d07766d611f7e52ccb9c1fe0e9b131a3510720
bryce <brycecrichfield@gmail.com>
2023-11-10 15:56:02 -0500

    added new background for links and formspree stub

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
-// // Navbar Autohide
-// var prevScrollpos = window.pageYOffset;
-// var timeout = null;
-// window.onscroll = function () {
-//     var currentScrollPos = window.pageYOffset;
-//     if (prevScrollpos != currentScrollPos) {
-//         document.querySelector(".navbar").style.top = "0";
-//         clearTimeout(timeout);
-//         timeout = setTimeout(function() {
-//             document.querySelector(".navbar").style.top = "-65px"; // adjust this value to match the height of your navbar
-//         }, 750);
-//     } 
-//     else {
-//         document.querySelector(".navbar").style.top = "-65px"; // adjust this value to match the height of your navbar
-//         clearTimeout(timeout);
-//     }
-//     prevScrollpos = currentScrollPos;
-// };
+// Navbar Autohide is only enabled on screens larger than 768px
+if (window.matchMedia("(min-width: 768px)").matches) {
+  // Navbar Hides after Scrolling
+  var prevScrollpos = window.pageYOffset;
+  var timeout = null;
+  window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos != currentScrollPos) {
+      document.querySelector(".navbar").style.top = "0";
+      clearTimeout(timeout);
+      timeout = setTimeout(function () {
+        document.querySelector(".navbar").style.top = "-65px"; // adjust this value to match the height of your navbar
+      }, 750);
+    }
+    else {
+      document.querySelector(".navbar").style.top = "-65px"; // adjust this value to match the height of your navbar
+      clearTimeout(timeout);
+    }
+    prevScrollpos = currentScrollPos;
+  };
 
-// $(document).ready(function () {
-//   $(".navbar").mouseenter(function () {
-//     $(this).css("top", "0");
-//   });
+  // Navbar Shows on Hover
+  $(document).ready(function () {
+    $(".navbar").mouseenter(function () {
+      $(this).css("top", "0");
+    });
 
-//   $(".navbar").mouseleave(function () {
-//     $(this).css("top", "-65px"); // adjust this value to match the height of your navbar
-//   });
-// });
+    $(".navbar").mouseleave(function () {
+      $(this).css("top", "-65px"); // adjust this value to match the height of your navbar
+    });
+  });
+}
